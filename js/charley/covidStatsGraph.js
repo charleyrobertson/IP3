6dd7c463f97dda44b6ce6edd17fa247862d2c000
charleyrobertson <weechazaaa1@gmail.com>
2021-03-08 14:13:00 +0000

    Changing chart details

--- js/charley/covidStatsGraph.js
+++ js/charley/covidStatsGraph.js
@@ -1,10 +1,22 @@
 $(document).ready(function () {
-  var ctx = document.getElementById("myDoughnutChart").getContext("2d");
+  var ctx = document.getElementById("doughnut-chart").getContext("2d");
   var myDoughnutChart = new Chart(ctx, {
     type: 'doughnut',
     data = {
         datasets: [{
-            data: [10, 20, 30]
+            label: 'Coronavirus Statistics',
+            data: [10, 20, 30],
+            backgroundColor: [
+              'rgba(255, 99, 132, 0.2)',
+              'rgba(54, 162, 235, 0.2)',
+              'rgba(255, 206, 86, 0.2)'
+          ],
+          borderColor: [
+              'rgba(255, 99, 132, 1)',
+              'rgba(54, 162, 235, 1)',
+              'rgba(255, 206, 86, 1)'
+          ],
+          borderWidth: 1
         }],
     
         // These labels appear in the legend and in the tooltips when hovering different arcs
