f7490cb195f37698b4db91e4f6a5c33e698acfab
JohnCRMoore <johncrmoore47808@gmail.com>
2021-03-08 11:08:21 +0000

    Removed keys that should have NEVER BEEN IN A REPO

--- js/john/spotify.js
+++
@@ -1,5 +0,0 @@
-/* var client_id = '1f3dd099f13c4bda9a0361f546f42367'; // Your client id
-var client_secret = 'f4ef3a06698f4118a6d933613def07d0'; // Your secret
-var redirect_uri = 'REDIRECT_URI'; // Your redirect uri */
-
-//placeholder
