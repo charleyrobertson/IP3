f8b4a5ce3dbcdd047ca864129fee711c7c2f7e5e
EmporerEmu <47246665+EmporerEmu@users.noreply.github.com>
2021-03-22 12:54:04 +0000

    Added nav functionality to iss.html

--- js/john/words.js
+++ js/john/words.js
@@ -13,7 +13,7 @@ $(document).ready(function () {
                 "url": url,
                 "method": "GET",
                 "headers": {
-                    "x-rapidapi-key": "???",
+                    "x-rapidapi-key": "3802d49eebmsh116ee2705f1741ep15ef6ejsn83b8aefe81f7",
                     "x-rapidapi-host": "wordsapiv1.p.rapidapi.com"
                 }
             };
@@ -34,7 +34,7 @@ $(document).ready(function () {
                     const iterationDef = {
                         definition: responseData.results[i].definition
                     }
-                    //console.log(iterationDef);
+                    console.log(iterationDef);
 
                     var definition = iterationDef.toString();
                     const div = document.createElement('div');
@@ -50,9 +50,9 @@ $(document).ready(function () {
                         const specificSyn = {
                             synonyms: responseData.results[i].synonyms[p]
                         }
-                        //console.log(specificSyn);
+                        console.log(specificSyn);
                     }
-                    //console.log("-----------");
+                    console.log("-----------");
 
                     
                 }
