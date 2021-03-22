41be3c223bd8477d6147948be439b82b05f96774
EmporerEmu <47246665+EmporerEmu@users.noreply.github.com>
2021-03-22 09:05:03 +0000

    Removed testing files for the NBA visualisations
    Removed un-used testing files from the application
    Navigation links were also fixed to point to the new areas of the
    application

--- js/jamie/nba.js
+++ js/jamie/nba.js
@@ -1,87 +1,213 @@
-const settings = {
-  async: true,
-  crossDomain: true,
-  url: "https://api-nba-v1.p.rapidapi.com/teams/players/teamId/",
-  method: "GET",
-  headers: {
-    "x-rapidapi-key": "",
-    "x-rapidapi-host": "api-nba-v1.p.rapidapi.com",
-  },
-};
-
-// function is required to automatically fire the JavaScript on page load
 $(document).ready(function () {
-  // Filling first two dropdown lists with static data
-  // fullName: teamId:
-  var list = document.getElementById("teamList");
-
-  list.options[0] = new Option("Atlanta Hawks", "1");
-  list.options[1] = new Option("Boston Celtics", "2");
-  list.options[2] = new Option("Brooklyn Nets", "4");
-  list.options[3] = new Option("Charlotte Hornets", "5");
-  list.options[4] = new Option("Chicago Bulls", "6");
-  list.options[5] = new Option("Cleveland Cavaliers", "7");
-  list.options[6] = new Option("Dallas Mavericks", "8");
-  list.options[7] = new Option("Denver Nuggets", "9");
-  list.options[8] = new Option("Detroit Pistons", "10");
-  list.options[9] = new Option("Golden State Warriors", "11");
-  list.options[10] = new Option("Houston Rockets", "14");
-  list.options[11] = new Option("Indiana Pacers", "15");
-  list.options[12] = new Option("LA Clippers", "16");
-  list.options[13] = new Option("Los Angeles Lakers", "17");
-  list.options[14] = new Option("Memphis Grizzlies", "19");
-  list.options[15] = new Option("Miami Heat", "20");
-  list.options[16] = new Option("Milwaukee Bucks", "21");
-  list.options[17] = new Option("Minnesota Timberwolves", "22");
-  list.options[18] = new Option("New Orleans Pelicans", "23");
-  list.options[19] = new Option("New York Knicks", "24");
-  list.options[20] = new Option("Oklahoma City Thunder", "25");
-  list.options[21] = new Option("Orlando Magic", "26");
-  list.options[22] = new Option("Philadelphia 76ers", "27");
-  list.options[23] = new Option("Phoenix Suns", "28");
-  list.options[24] = new Option("Portland Trail Blazers", "29");
-  list.options[25] = new Option("Sacramento Kings", "30");
-  list.options[26] = new Option("San Antonia Spurs", "31");
-  list.options[27] = new Option("Toronto Raptors", "38");
-  list.options[28] = new Option("Utah Jazz", "40");
-  list.options[29] = new Option("Washington Wizards", "41");
-
-  // Seasons on rapid api are from 2015 onwards
   var seasonList = document.getElementById("seasonList");
-  seasonList.options[0] = new Option("2015-16", "");
-  seasonList.options[1] = new Option("2016-17", "");
-  seasonList.options[2] = new Option("2017-18", "");
-  seasonList.options[3] = new Option("2018-19", "");
-  seasonList.options[4] = new Option("2019-20", "");
-  seasonList.options[5] = new Option("2020-21", "");
+  seasonList.options[0] = new Option("Select a season...", "0");
+  seasonList.options[1] = new Option("2015-16", "");
+  seasonList.options[2] = new Option("2016-17", "");
+  seasonList.options[3] = new Option("2017-18", "");
+  seasonList.options[4] = new Option("2018-19", "");
+  seasonList.options[5] = new Option("2019-20", "");
+  seasonList.options[6] = new Option("2020-21", "");
 
-  getPlayers();
+  displaySeasonHeader();
+  getLebronJames();
+  playerSearch();
 });
 
-function getPlayers() {
-  // Populate player search on select action
-  $("#teamList").change(function () {
-    // Get team id from select and append to API url
-    var teamID = $("#teamList option:selected").val();
-    settings.url = "https://api-nba-v1.p.rapidapi.com/players/teamId/" + teamID;
-
-    $.ajax(settings).done(function (data) {
-      var playerList = document.getElementById("playerList");
-      console.log(data);
-
-      // Uses players first + last names as text, and playerId as value for select list
-      for (i = 0; i < data.api.players.length; i++) {
-        playerList.options[i] = new Option(
-          data.api.players[i].firstName + " " + data.api.players[i].lastName,
-          data.api.players[i].playerId
-        );
-      }
-    });
+// TODO:
+// Search for a player and append results to the datalist
+// when value is selected, data will be sent to charting functions
+// when value is selected, the datalist should clear all the options and values
+function playerSearch() {
+  $("#playerSearch").keypress(function () {
+    if ($(this).val().length > 4) {
+      console.log("Success!");
+      var q = $("#playerSearch").val();
+      q = q.replace(/ /g, "_");
+      console.log(q);
+      $.ajax("https://www.balldontlie.io/api/v1/players?search=" + q).done(
+        function (data) {
+          console.log(data);
+          var list = document.getElementById("playerSearchOptions");
+          for (i = 0; i < data.data.length; i++) {
+            console.log(data.data[i].first_name);
+            var option = document.createElement("option");
+            option.value =
+              data.data[i].first_name + " " + data.data[i].last_name;
+            list.appendChild(option);
+          }
+          console.log(list);
+        }
+      );
+    }
   });
+}
 
-  settings.url =
-    "https://api-nba-v1.p.rapidapi.com/statistics/players/playerId/92";
-  $.ajax(settings).done(function (data) {
-    console.log(data);
+// Will display a header on season selection
+function displaySeasonHeader() {
+  // Display Header based on season selection
+  $("#seasonList").change(function () {
+    var season = $("#seasonList option:selected").text();
+    var h1 = document.createElement("h1");
+    h1.innerHTML = season + " SEASON AVERAGES";
+    var div = document.getElementById("seasonHeader");
+    if (season !== "Select a season...") {
+      $("#seasonHeader").empty();
+      div.append(h1);
+    }
+  });
+}
+
+// A hardcoded function for testing
+// Will be used for multiple players and charts/tables
+function getLebronJames() {
+  // Getting the goats stats for selected season
+  // Only get stats if default isnt selected
+  $("#seasonList").change(function () {
+    var season = $("#seasonList option:selected").text();
+    if (season !== "Select a season...") {
+      season = season.substring(0, 4);
+      // console.log(season);
+      $.ajax(
+        "https://www.balldontlie.io/api/v1/season_averages?season=" +
+          season +
+          "&player_ids[]=237"
+      ).done(function (data) {
+        console.log("Lebron James's season averages: ", data);
+        mapStatsToChart(data);
+        getFirstSeason();
+      });
+    }
+  });
+}
+
+// Getting the first season a player played in
+function getFirstSeason() {
+  $.ajax("https://www.balldontlie.io/api/v1/stats?player_ids[]=237").done(
+    function (data) {
+      var firstSeason = parseInt(data.data[0].game.season, 10);
+      console.log("The players first season: ", firstSeason);
+      calcCareerAverage(firstSeason);
+    }
+  );
+}
+
+// Calculating a players career averages in chosen metrics
+async function calcCareerAverage(firstSeason) {
+  var careerStats = {
+    points: 0.0,
+    assists: 0.0,
+    rebounds: 0.0,
+    blocks: 0.0,
+    steals: 0.0,
+    turnOver: 0.0,
+  };
+  var counter = 0;
+
+  for (i = firstSeason; i < 2020; i++) {
+    $.ajax({
+      url:
+        "https://www.balldontlie.io/api/v1/season_averages?season=" +
+        i +
+        "&player_ids[]=237",
+      // async: false,
+    }).done(function (data) {
+      careerStats.points += data.data[0].pts;
+      careerStats.assists += data.data[0].ast;
+      careerStats.rebounds += data.data[0].reb;
+      careerStats.blocks += data.data[0].blk;
+      careerStats.steals += data.data[0].stl;
+      careerStats.turnOver += data.data[0].turnover;
+      console.log(careerStats.points);
+    });
+
+    counter++;
+    console.log(counter);
+  }
+}
+
+function consoleLogActuallyRuinedMyEvening(careerStats) {
+  console.log(careerStats);
+}
+
+var ctx = null;
+var chart = null;
+
+function mapStatsToChart(data) {
+  const labels = [];
+  const seasonStats = {
+    points: data.data[0].pts,
+    assits: data.data[0].ast,
+    rebounds: data.data[0].reb,
+    blocks: data.data[0].blk,
+    steals: data.data[0].stl,
+    turnOver: data.data[0].turnover,
+  };
+  console.log("Lebrons chosen season stats: ", seasonStats);
+
+  var ctx = document.getElementById("seasonAverages").getContext("2d");
+
+  // Destroy previous chart
+  if (chart != null) {
+    chart.destroy();
+    chart = null;
+  }
+
+  chart = new Chart(ctx, {
+    type: "bar",
+    data: {
+      labels: [
+        "Points",
+        "Assists",
+        "Rebounds",
+        "Blocks",
+        "Steals",
+        "Turn Overs",
+      ],
+      datasets: [
+        {
+          label: "Season Averages",
+          data: [
+            seasonStats.points,
+            seasonStats.assits,
+            seasonStats.rebounds,
+            seasonStats.blocks,
+            seasonStats.steals,
+            seasonStats.turnOver,
+          ],
+          backgroundColor: [
+            "rgba(255, 99, 132, 0.2)",
+            "rgba(54, 162, 235, 0.2)",
+            "rgba(255, 206, 86, 0.2)",
+            "rgba(75, 192, 192, 0.2)",
+            "rgba(153, 102, 255, 0.2)",
+            "rgba(255, 159, 64, 0.2)",
+          ],
+          borderColor: [
+            "rgba(255, 99, 132, 1)",
+            "rgba(54, 162, 235, 1)",
+            "rgba(255, 206, 86, 1)",
+            "rgba(75, 192, 192, 1)",
+            "rgba(153, 102, 255, 1)",
+            "rgba(255, 159, 64, 1)",
+          ],
+          borderWidth: 1,
+        },
+        {
+          label: "Career Averages",
+          data: [],
+        },
+      ],
+    },
+    options: {
+      scales: {
+        yAxes: [
+          {
+            ticks: {
+              beginAtZero: true,
+            },
+          },
+        ],
+      },
+    },
   });
 }
