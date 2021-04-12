dc9c954ef4d98df24ccf7e0b7b062076cbe61975
JohnCRMoore <johncrmoore47808@gmail.com>
2021-04-12 12:11:22 +0100

    Updated JS file to fix/update the chart and its general functionality.

--- js/john/words.js
+++ js/john/words.js
@@ -19,8 +19,7 @@ $(document).ready(function () {
 				url: url,
 				method: "GET",
 				headers: {
-					"x-rapidapi-key":
-						"a654b2db95msh1c6ca0fca5ebd64p1752c3jsn452f27def07d",
+					"x-rapidapi-key": "",
 					"x-rapidapi-host": "wordsapiv1.p.rapidapi.com",
 				},
 			};
@@ -53,18 +52,11 @@ $(document).ready(function () {
 					//Gets the current definition and turns it into a string variable.
 					var definition = iterationDef.definition.toString();
 
-					/* !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! */
-					var defArray = defArray.concat(definition);
-
 					//Dynamically creates an element on the HTML page that displays the definition alongside it's exact iteration number.
+					var defArray = defArray.concat(definition);
 					const div = document.createElement("div");
 					div.className = "row";
-					div.innerHTML =
-						"<h6><b>Definition No." +
-						[i] +
-						':</b></h6><br><p>"' +
-						definition +
-						'"</p>';
+					div.innerHTML = "<h6><b>Definition No." + [i] + ':</b></h6><br><p>"' + definition + '"</p>';
 					document.getElementById("apiDef").appendChild(div);
 
 					//Gets the synonyms associated with the current definition.
@@ -80,11 +72,8 @@ $(document).ready(function () {
 						var defSynonymLength =
 							responseData.results[i].synonyms.length;
 
-						/* !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! */
 						var synLengthInt = parseInt(defSynonymLength);
-						var synLengthArray = synLengthArray.concat(
-							synLengthInt
-						);
+						var synLengthArray = synLengthArray.concat(synLengthInt);
 
 						//Runs the following code for however many synonyms are associated with the definition.
 						for (p = 0; p < defSynonymLength; p++) {
@@ -97,82 +86,34 @@ $(document).ready(function () {
 							var specSynonym = specificSyn.synonyms.toString();
 							const div = document.createElement("div");
 							div.className = "row";
-							div.innerHTML =
-								"<p>- <i>" + specSynonym + "</i></p>";
+							div.innerHTML = "<p>- <i>" + specSynonym + "</i></p>";
 							document.getElementById("apiDef").appendChild(div);
 						}
 					} else {
-						var synLengthArray = synLengthArray.concat(
-							synLengthInt
-						);
+						var synLengthArray = synLengthArray.concat(0);
 					}
-					console.log(defArray);
-					console.log(synLengthArray);
 				}
 
-				// function drawChart(socCode) {
-				// 	var wfURL =
-				// 		"http://api.lmiforall.org.uk/api/v1/wf/predict?soc=";
-				// 	$.getJSON(wfURL + socCode, function (data) {
-				// 		var chartData = {
-				// 			labels: [],
-				// 			datasets: [
-				// 				{
-				// 					label: "Predicted Employment",
-				// 					data: [],
-				// 				},
-				// 			],
-				// 		};
-
-				// 		data.predictedEmployment.forEach(function (yearData) {
-				// 			chartData.labels.push(yearData.year);
-				// 			chartData.datasets[0].data.push(
-				// 				yearData.employment
-				// 			);
-				// 		});
-
-				// 		if (chart != null) {
-				// 			chart.destroy();
-				// 			chart = null;
-				// 		}
-				// 		chart = new Chart(ctx).Line(chartData);
-				// 	});
-				// }
-
-				var chartData = {
-					labels: [],
-					datasets: [
-						{
-							label: "Some words",
-							data: [],
-						},
-					],
-				};
-
-                // for(i=0; i<synLengthArray.length; i++) {
-                //     chartData.datasets[i].data.push(synLengthArray[i]);
-                //     console.log(chartData.datasets);
-                // }
-				synLengthArray.forEach(function (data) {
-					chartData.datasets[0].data.push(data);
-					console.log(chartData.datasets);
-				});
-
 				var data = {
-					datasets: [
-						{
-							data: [24, 9],
+					datasets: [{
+							data: synLengthArray,
 							backgroundColor: [
-								"#000000",
-								"#3B5998",
-								"#a6b1b7",
-								"#1da1f2",
-								"#bd081c",
+								"#FF0000",
+								"#FF7F00",
+								"#FFFF00",
+								"#00FF00",
+								"#0000FF",
+								"#4B0082",
+								"#9400D3",
+								"#232323",
+								"#666565",
+								"#A7A6A6",
+								"#DDDDDD"
 							],
-							// label: "Words & Synonyms:",
+							label: "Words & Synonyms:",
 						},
 					],
-					labels: [defArray],
+					labels: defArray,
 				};
 				var ctx = $("#wordDefSynChart");
 				new Chart(ctx, {
@@ -184,10 +125,6 @@ $(document).ready(function () {
 						},
 						scale: {
 							display: true,
-							y: {
-								suggestedMin: 1,
-								suggestedMax: 15,
-							},
 						},
 					},
 				});
